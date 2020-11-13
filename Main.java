526178b2667cc6f3afe7dc3128b05b26b8a57e75
Robert Szlufik <robert.szlufik123@gmail.com>
2020-11-13 11:49:24 +0000

    Project for Time and Task
    have a look inside, there is some code that might help to organize it a bit better

---
+++ Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        // to call a method
+        // <yourMethodName>();
+        example();
+
+
+
+    }
+    public static void yourMethod(){
+       /* Add your code here
+        it might be whatever you need
+        youre gonna create new method
+        and then add it to the main method
+        that will make program much more coherent
+        */
+    }
+
+     public static void example(){
+         System.out.println("Example method");
+     }
+}
+
